26caacdcd665a1829f02be5fff8260ba4f6c5b0d
AllenWilson <tcaw.super@gmail.com>
2022-11-09 16:16:10 +0800

    init files

---
+++ src/main/java/com/example/itr/IdeaTestRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.itr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaTestRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaTestRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itr/IdeaTestRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaTestRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
